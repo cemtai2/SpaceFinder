6b959cdeccb23582c8a1865b722dd1e8a3b3225e
WMDesign <faile486@gmail.com>
2014-01-16 15:48:14 -0600

    v 01

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body>
+  <header>
+		<h1>
+  		SpaceRater.com
+		</h1> <br />
+		
+    <nav>
+    	<a href='#'>Add New</a>
+      <a href='#'>View</a>
+      <a href='#'>About</a>
+      <a href='#'>How To</a>
+		</nav>
+	</header>
+
+	<section>
+  	<h2>Welcome to SpaceFinder!</h2>
+    
+    <p>
+    	SpaceFinder is an app designed to help you find a space to live based on criteria that you provide.  We've added as many criteria as we can think of - all you need to do is rate their importance to you and then enter your potential living spaces.  SpaceFinder will rank them accordingly.
+    </p>
+  </section>
+  
+  <section>
+  	<h2>How To: </h2>
+		
+    <ul>
+    	<li>Use our easy form to rate each criteria in order of importance to you.</li>
+      <li>Add all the places that you are considering.</li>
+      <li>COMPUTER MAGIC</li>
+      <li>You now have a neat, ranked list of places!</li>
+    </ul>
+  </section>
+</body>
+
+</html>
+
+<!--
+PHP Function List
+Display Listings - Prints a list of all listings by rating
+Sort Listings - Sort listings by rating, sqft, distance
+Add Listing - Brings up form to add a new listing
+Save Listing - Saves a new or edited listing
+Edit Listing - Bring up form to edit a listing
+Delete Listing - Confirms and deletes a listing
+Calculate Value - Performs rating calculations
+Form Validation - Validates form data
+Form - The form
+-->
