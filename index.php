4ae2183a59b85f9f23fed7e21b88c98d38db373e
WMDesign <faile486@gmail.com>
2014-01-17 12:20:24 -0600

    Base

--- index.php
+++ index.php
@@ -2,13 +2,17 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Title of the document</title>
+<title>SpaceFinder</title>
+<link rel='stylesheet/less' type='text/css' href='styles/stylesheet.less' />
+<link rel='stylesheet' type='text/css' href='styles/fonts.css'>
+
+<script src="js/less.js" type="text/javascript"></script>
 </head>
 
 <body>
   <header>
 		<h1>
-  		SpaceRater.com
+  		SpaceFinder.com
 		</h1> <br />
 		
     <nav>
@@ -19,24 +23,29 @@
 		</nav>
 	</header>
 
-	<section>
-  	<h2>Welcome to SpaceFinder!</h2>
+	<div id='content'>
+    <section>
+      <h2>Welcome to SpaceFinder!</h2>
+      
+      <p>
+        SpaceFinder is an app designed to help you find a space to live based on criteria that you provide.  We've added as many criteria as we can think of - all you need to do is rate their importance to you and then enter your potential living spaces.  SpaceFinder will rank them accordingly.
+      </p>
+    </section>
     
-    <p>
-    	SpaceFinder is an app designed to help you find a space to live based on criteria that you provide.  We've added as many criteria as we can think of - all you need to do is rate their importance to you and then enter your potential living spaces.  SpaceFinder will rank them accordingly.
-    </p>
-  </section>
+    <section>
+      <h2>How To: </h2>
+      
+      <ul>
+        <li>Use our easy form to rate each criteria in order of importance to you.</li>
+        <li>Add all the places that you are considering.</li>
+        <li>COMPUTER MAGIC</li>
+        <li>You now have a neat, ranked list of places!</li>
+      </ul>
+    </section>
+	</div>
   
-  <section>
-  	<h2>How To: </h2>
-		
-    <ul>
-    	<li>Use our easy form to rate each criteria in order of importance to you.</li>
-      <li>Add all the places that you are considering.</li>
-      <li>COMPUTER MAGIC</li>
-      <li>You now have a neat, ranked list of places!</li>
-    </ul>
-  </section>
+  <footer>
+  </footer>
 </body>
 
 </html>
